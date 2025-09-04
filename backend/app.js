36de55d3e27c17cdf38ce35c126b3fce029226c4
rommel dalisay <rommelmdalisay@gmail.com>
2025-09-04 16:17:35 +0800

    new product

--- backend/app.js
+++ backend/app.js
@@ -3,13 +3,17 @@ const app = express();
 const cors = require('cors')
 const cookieParser = require('cookie-parser')
 
-
+const products = require('./routes/product');
 
 
 app.use(express.json({limit:'50mb'}));
 app.use(express.urlencoded({limit: "50mb", extended: true }));
 app.use(cors());
 app.use(cookieParser());
+
+app.use('/api/v1', products);
+
+
 
 
 
