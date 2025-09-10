2ba324b372c27fa57f92cd0922534f7d4a0a3fa4
rommel dalisay <rommelmdalisay@gmail.com>
2025-09-10 09:39:51 +0800

    home

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,25 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+
 import './App.css'
+import Header from './Components/Layout/Header';
+import Footer from './Components/Layout/Footer';
+
+import Home from './Components/Home';
 
 function App() {
-  
+
 
   return (
     <>
-      
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} exact="true" />
+        </Routes>
+        <Footer />
+      </Router>
     </>
   )
 }
