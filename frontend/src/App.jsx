c059d1e1f2136fcd38708865dffc9f900ecd0d8e
rommel dalisay <rommelmdalisay@gmail.com>
2025-09-11 13:44:52 +0800

    product details

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,7 @@ import Header from './Components/Layout/Header';
 import Footer from './Components/Layout/Footer';
 
 import Home from './Components/Home';
+import ProductDetails from './Components/Product/ProductDetails';
 
 function App() {
 
@@ -17,6 +18,7 @@ function App() {
         <Header />
         <Routes>
           <Route path="/" element={<Home />} exact="true" />
+          <Route path="/product/:id" element={<ProductDetails />} exact="true" />
         </Routes>
         <Footer />
       </Router>
--- frontend/src/Components/Product/Product.jsx
+++ frontend/src/Components/Product/Product.jsx
@@ -6,7 +6,7 @@ const Product = ({product}) => {
             <div className="card p-3 rounded">
                 <img
                     className="card-img-top mx-auto"
-                    src={product.images[0] ? product.images[0].url : ''}
+                    src={product.images[0] ? product.images[0].url : null}
                 />
                 <div className="card-body d-flex flex-column">
                     <h5 className="card-title">
